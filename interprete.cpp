51059876409ec6112f01d034b48d7b253c4f1ba2
Avril <tatiana28sosa20@gmail.com>
2025-10-29 20:23:05 -0300

    interprete.cpp

--- interprete.cpp
+++ interprete.cpp
@@ -10,11 +10,12 @@ bool Interprete::getEstado(){return estado;}
 
 void Interprete::setNombre(const char* _nombre){strcpy(nombre, _nombre);}
 void Interprete::setNacionalidad(const char* _nacionalidad){strcpy(nacionalidad, _nacionalidad);}
-void Interprete::setSuscriptores(int _suscriptores){strcpy(suscriptores, _suscriptores);}
+void Interprete::setSuscriptores(int _suscriptores){suscriptores=_suscriptores;}
 void Interprete::setEstado(bool _estado){estado=_estado;}
 void Interprete::setNotificaciones(bool _notificaciones){notificaciones=_notificaciones;}
 
 void cargarInterprete();
 void mostrarInterprete();
+
 
 
