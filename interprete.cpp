93a4a90ebe8f1a5a78e148f505560fe128b8fdee
Avril <tatiana28sosa20@gmail.com>
2025-10-29 20:19:27 -0300

    interprete.cpp

--- interprete.cpp
+++ interprete.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 int Interprete::getIDInterprete(){return idInterprete;}
-char* getNombre();
+char* getNombre(){return }
 int getSuscriptores();
 bool getNotificaciones();
 bool getEstado();
 
-void setNombre(const char* _nombre);
-void setNacionalidad(const char* _nacionalidad);
-void setSuscriptores(int _suscriptores);
-void setEstado(bool _estado);
-void setNotificaciones(bool _notificaciones);
+void Interprete::setNombre(const char* _nombre){strcpy(nombre, _nombre);}
+void Interprete::setNacionalidad(const char* _nacionalidad){strcpy(nacionalidad, _nacionalidad);}
+void Interprete::setSuscriptores(int _suscriptores){strcpy(suscriptores, _suscriptores);}
+void Interprete::setEstado(bool _estado){estado=_estado;}
+void Interprete::setNotificaciones(bool _notificaciones){notificaciones=_notificaciones;}
 
 void cargarInterprete();
 void mostrarInterprete();
+
