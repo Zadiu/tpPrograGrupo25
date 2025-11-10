932e93c4100f3be249e700b543cf9b5a190aab2d
Avril <tatiana28sosa20@gmail.com>
2025-11-10 17:15:41 -0300

    Update manager.h

--- manager.h
+++ manager.h
@@ -2,6 +2,9 @@
 
 class Manager{
 public:
-    void intentarRegistro(const char* nombre, const char* apellido, const char* dni, const char* mail, const char* telefono);
-    void intentarLogin(int ID, const char* mail);
+    //son booleanas pq devuelven true o false si la operacion se llevo a cabo con exito
+    bool intentarRegistro(int idUsuario, const char* nombre, const char* apellido, const char* dni, const char* mail, const char* telefono);
+    bool intentarLogin(int ID, const char* mail);
+    bool cargarDatosDesdeArchivos();
+    void agregarUsuario(const Usuario& nuevoUsuario);
 };
